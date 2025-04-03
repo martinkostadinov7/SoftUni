46f3a791efc1b580b2eee62427a898a27c9ed1d2
Martin Kostadinov <m.kostadinov07@gmail.com>
2025-04-03 16:15:53 +0300

    Add initial project structure and sample program
    Created a new Visual Studio solution file (`Intro-To-Programming.sln`) and project file (`Intro-To-Programming.csproj`) targeting .NET 9.0. Added a simple C# program in `Program.cs` that prints "Good Day!" to the console.

---
+++ c#-book-nakov/Intro-To-Programming/Intro-To-Programming/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Intro_To_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Day!");
+        }
+    }
+}
