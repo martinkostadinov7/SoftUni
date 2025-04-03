93005fde0ce744eec6d34a1354a10e28d35deae5
Martin Kostadinov <m.kostadinov07@gmail.com>
2025-04-03 16:18:39 +0300

    Add WriteMyName project with console output
    Created `WriteMyName.cs` with a `Program` class that
    outputs "Martin Kostadinov" to the console. Added
    `WriteMyName.csproj` with .NET 9.0 configuration,
    implicit usings, and nullable reference types enabled.

---
+++ c#-book-nakov/Intro-To-Programming/WriteMyName/WriteMyName.cs
@@ -0,0 +1,10 @@
+﻿namespace WriteMyName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Martin Kostadinov");
+        }
+    }
+}
