bbc6eda8854ab32b0b37db92b9f8d274a3625095
martinkostadinov7 <m.kostadinov07@gmail.com>
2025-04-04 22:03:40 +0300

    Add IsPointInsideCircle and IsPointInsideCirlceAndRectangle projects
    Updated the solution to include two new projects:
    - `IsPointInsideCircle` with a `PointChecker` class that checks if a point is inside a circle.
    - `IsPointInsideCirlceAndRectangle` with a `PointChecker` class that checks if a point is inside both a circle and a rectangle.
    
    Both projects target .NET 9.0 and include necessary configurations in their respective `.csproj` files.

---
+++ c#-book-nakov/Operators and Expressions/IsPointInsideCircle/PointChecker.cs
@@ -0,0 +1,15 @@
+﻿namespace IsPointInsideCircle
+{
+    internal class PointChecker
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int R = int.Parse(Console.ReadLine());
+
+            bool isInside = R * R >= x * x + y * y;
+            Console.WriteLine(isInside);
+        }
+    }
+}
---
+++ c#-book-nakov/Operators and Expressions/IsPointInsideCirlceAndRectangle/PointChecker.cs
@@ -0,0 +1,21 @@
+﻿namespace IsPointInsideCirlceAndRectangle
+{
+    internal class PointChecker
+    {
+        static void Main(string[] args)
+        {
+
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            const int r = 5;
+            const int x1 = -1;
+            const int y1 = -1;
+            const int x2 = 5;
+            const int y2 = 5;
+
+            bool isInsideBoth = (r * r >= x * x + y * y) && ((x >= x1 && y >= y1) && (x <= x2 && y <= y2));
+            Console.WriteLine(isInsideBoth);
+        }
+    }
+}
