bd733b2fddeba4fd301a146c13180a1392cc6496
martinkostadinov7 <m.kostadinov07@gmail.com>
2025-04-04 21:56:58 +0300

    Add IsThirdNumber7 project and implementation
    Updated the solution to include a new project "IsThirdNumber7" with its associated project file. The project targets .NET 9.0 and is configured as an executable.
    
    Added a new source file `ThirdNumCheck.cs` that implements functionality to check if the second digit of an input integer is 7 and outputs the result.

---
+++ c#-book-nakov/Operators and Expressions/IsThirdNumber7/ThirdNumCheck.cs
@@ -0,0 +1,13 @@
+﻿namespace IsThirdNumber7
+{
+    internal class ThirdNumCheck
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int n1 = n / 100;
+            bool is7 = n1 % 10 == 7;
+            Console.WriteLine(is7);
+        }
+    }
+}
