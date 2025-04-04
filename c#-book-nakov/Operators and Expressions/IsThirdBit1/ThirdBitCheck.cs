049a70345d816ea5a2644e3e0e14d8afbdedc00a
martinkostadinov7 <m.kostadinov07@gmail.com>
2025-04-04 21:58:21 +0300

    Add IsThirdBit1 and TrapezoidArea projects
    Updated solution to include two new projects: "IsThirdBit1" and "TrapezoidArea", each with their own .csproj files. Implemented `ThirdBitCheck` class to check the third bit of an integer and `TrapeziodArea` class to calculate the area of a trapezoid. Both projects target .NET 9.0.

---
+++ c#-book-nakov/Operators and Expressions/IsThirdBit1/ThirdBitCheck.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IsThirdBit1
+{
+    internal class ThirdBitCheck
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool bit3 = (n & 8) != 0;
+            Console.WriteLine(bit3);
+        }
+    }
+}
---
+++ c#-book-nakov/Operators and Expressions/TrapezoidArea/TrapeziodArea.cs
@@ -0,0 +1,14 @@
+﻿namespace TrapezoidArea
+{
+    internal class TrapeziodArea
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(((a + b) / 2) * h);
+        }
+    }
+}
