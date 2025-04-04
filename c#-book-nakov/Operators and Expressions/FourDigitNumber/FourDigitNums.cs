6517164f6d9c27a23336c18927cccd8d6be533c0
martinkostadinov7 <m.kostadinov07@gmail.com>
2025-04-04 22:04:42 +0300

    Add FourDigitNumber project and implementation
    This commit introduces a new project named "FourDigitNumber" to the solution, including its configuration settings in a new `.csproj` file. Additionally, a source file `FourDigitNums.cs` is added, which implements a program that reads a four-digit integer, calculates the sum of its digits, and displays various permutations of those digits.

---
+++ c#-book-nakov/Operators and Expressions/FourDigitNumber/FourDigitNums.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FourDigitNumber
+{
+    internal class FourDigitNums
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int d = n % 10;
+            n /= 10;
+            int c = n % 10;
+            n /= 10;
+            int b = n % 10;
+            n /= 10;
+            int a = n % 10;
+
+
+            int sum = a + b + c + d;
+            Console.WriteLine(sum);
+
+            Console.WriteLine($"{d}{c}{b}{a}");
+
+            Console.WriteLine($"{d}{a}{b}{c}");
+
+            Console.WriteLine($"{a}{c}{b}{d}");
+        }
+    }
+}
