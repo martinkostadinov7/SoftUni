1ae4372726c1756230ed4ead5e419f322a85efcd
martinkostadinov7 <m.kostadinov07@gmail.com>
2025-04-04 22:01:08 +0300

    Add RectangleArea and MoonGravity projects
    Updated the solution file to include new projects: RectangleArea and MoonGravity, each with their own .csproj files. Added configuration settings for Debug and Release modes. Created project files targeting .NET 9.0 with necessary settings. Implemented main classes for calculating moon weight and rectangle area/perimeter.

---
+++ c#-book-nakov/Operators and Expressions/MoonGravity/MoonGravityCalculator.cs
@@ -0,0 +1,12 @@
+﻿namespace MoonGravity
+{
+    internal class MoonGravityCalculator
+    {
+        static void Main(string[] args)
+        {
+            double earthKg = double.Parse(Console.ReadLine());
+            double moonKg = earthKg - earthKg * 0.17;
+            Console.WriteLine(moonKg);
+        }
+    }
+}
---
+++ c#-book-nakov/Operators and Expressions/RectangleArea/RectangleArea.cs
@@ -0,0 +1,17 @@
+﻿namespace RectangleAreaPer
+{
+    internal class RectangleArea
+    {
+        static void Main(string[] args)
+        {
+
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            double area = a * b;
+            double perimeter = 2 * (a + b);
+            Console.WriteLine(area);
+            Console.WriteLine(perimeter);
+        }
+    }
+}
