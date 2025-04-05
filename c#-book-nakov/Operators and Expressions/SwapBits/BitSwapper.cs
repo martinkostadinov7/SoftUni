dfa0fa03917e774d0be3d664eb2d85315a040b1c
Martin Kostadinov <m.kostadinov07@gmail.com>
2025-04-05 16:36:59 +0300

    Add SwapBits and SwapThirdFifthBit projects
    Updated solution file to include new projects: SwapBits and SwapThirdFifthBit, each with their own .csproj files. Added functionality to swap specified bits in integer inputs. Configured build settings for Debug and Release modes in the solution. Both projects target .NET 9.0 with implicit usings and nullable reference types enabled.

---
+++ c#-book-nakov/Operators and Expressions/SwapBits/BitSwapper.cs
@@ -0,0 +1,21 @@
+﻿namespace SwapBits
+{
+    internal class BitSwapper
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int p1 = int.Parse(Console.ReadLine());
+            int p2 = int.Parse(Console.ReadLine());
+
+            int bitP1 = (n >> p1) & 1; // намиране на бит на позиция p1
+            int bitP2 = (n >> p2) & 1;
+
+            n = n & (~(1 << p2)) | (bitP1 << p2); // n & (~(1<<p2)) - изчиства бита на позиция p2, | (bitP1 << p2) - 
+            n = n & (~(1 << p1)) | (bitP2 << p1);
+
+            Console.WriteLine(n);
+        }
+    }
+}
---
+++ c#-book-nakov/Operators and Expressions/SwapThirdFifthBit/BitSwapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SwapThirdFifthBit
+{
+    internal class BitSwapper
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int bit2 = (n >> 2) & 1;
+            int bit5 = (n >> 5) & 1;
+
+            n = n & (~(1 << 5)) | (bit2 << 5);
+            n = n & (~(1 << 2)) | (bit5 << 2);
+
+
+            Console.WriteLine(n);
+        }
+    }
+}
