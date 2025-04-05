4b0459bb522d22c0883334792a69fb0613b77e2c
Martin Kostadinov <m.kostadinov07@gmail.com>
2025-04-05 16:35:22 +0300

    Add ChangePBit and IsNumPrime projects to solution
    Updated the solution file `Operators and Expressions.sln` to include two new projects: `ChangePBit` and `IsNumPrime`. The `ChangePBit` project features a `BitChanger` class for modifying specific bits of an integer, while the `IsNumPrime` project contains a `PrimeChecker` class for determining if a number is prime. Both projects target .NET 9.0 and have been configured with implicit usings and nullable reference types.

---
+++ c#-book-nakov/Operators and Expressions/ChangePBit/BitChanger.cs
@@ -0,0 +1,17 @@
+﻿namespace ChangePBit
+{
+    internal class BitChanger
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            int v = int.Parse(Console.ReadLine());
+
+            int newN = (v == 1) ? n | (1 << p) : n & (~(1 << p));
+            Console.WriteLine(newN);
+
+        }
+    }
+}
---
+++ c#-book-nakov/Operators and Expressions/IsNumPrime/PrimeChecker.cs
@@ -0,0 +1,19 @@
+﻿namespace IsNumPrime
+{
+    internal class PrimeChecker
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            bool isPrime = false;
+
+            for (int i = 1; i < Math.Sqrt(n); i++)
+            {
+                isPrime = (n % i == 0) ? false : true;
+            }
+
+            Console.WriteLine(isPrime);
+        }
+    }
+}
