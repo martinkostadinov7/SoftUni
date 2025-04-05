4872d6c320bb37d083302f889911966951bf3383
Martin Kostadinov <m.kostadinov07@gmail.com>
2025-04-05 16:33:19 +0300

    Add PDigitBit and IsBitP1 projects to solution
    Updated the solution file to include two new projects: PDigitBit and IsBitP1, along with their respective project files. Created BitChecker.cs for both projects, implementing a Main method that checks if a specified bit is set in an integer. The IsBitP1 project outputs a boolean, while PDigitBit outputs an integer (0 or 1). Both projects are configured with the .NET SDK, targeting .NET 9.0, and enabling implicit usings and nullable reference types.

---
+++ c#-book-nakov/Operators and Expressions/IsBitP1/BitChecker.cs
@@ -0,0 +1,14 @@
+﻿namespace IsBitP1
+{
+    internal class BitChecker
+    {
+        static void Main(string[] args)
+        {
+            int v = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+
+            bool isBit1 = ((v >> p) & 1) != 0;
+            Console.WriteLine(isBit1);
+        }
+    }
+}
---
+++ c#-book-nakov/Operators and Expressions/PDigitBit/BitChecker.cs
@@ -0,0 +1,15 @@
+﻿namespace PDigitBit
+{
+    internal class BitChecker
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+
+            bool bit = ((n >> p) & 1) != 0;
+            int isBit = bit ? 1 : 0;
+            Console.WriteLine(isBit);
+        }
+    }
+}
