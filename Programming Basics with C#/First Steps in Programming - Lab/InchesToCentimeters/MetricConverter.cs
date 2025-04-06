10534b51e54ae1b80e7b0b9039b746e07c539a15
Martin Kostadinov <m.kostadinov07@gmail.com>
2025-04-06 11:42:37 +0300

    Add new projects for area calculation and number counting
    Updated the solution to include three new projects: "NumbersToTen", "AreaOfRectangle", and "InchesToCentimeters".
    Created `AreaCalculator.cs` for calculating rectangle area, `MetricConverter.cs` for converting inches to centimeters, and `CountingToTen.cs` for printing numbers from 1 to 10.

---
+++ Programming Basics with C#/First Steps in Programming - Lab/InchesToCentimeters/MetricConverter.cs
@@ -0,0 +1,12 @@
+﻿namespace InchesToCentimeters
+{
+    internal class MetricConverter
+    {
+        static void Main(string[] args)
+        {
+            double inches = double.Parse(Console.ReadLine());
+            double centimeters = inches * 2.54;
+            Console.WriteLine(centimeters);
+        }
+    }
+}
