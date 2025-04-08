43086b82b15e84e6599f4121583300a9d6b403eb
Martin Kostadinov <m.kostadinov07@gmail.com>
2025-04-08 22:26:18 +0300

    Add VacationBookList and SuppliesForSchool projects
    Updated the solution to include two new projects:
    - `VacationBookList` with a program to calculate reading days.
    - `SuppliesForSchool` with a program to compute total school supply costs based on user input.

---
+++ Programming Basics with C#/First Steps in Programming - Exercise/SuppliesForSchool/SchoolSupplies.cs
@@ -0,0 +1,20 @@
+﻿namespace SuppliesForSchool
+{
+    internal class SchoolSupplies
+    {
+        static void Main(string[] args)
+        {
+            decimal penPrice = 5.80m;
+            decimal markerPrice = 7.20m;
+            decimal cleaningPrice = 1.20m;
+
+            int penCount = int.Parse(Console.ReadLine());
+            int markerCount = int.Parse(Console.ReadLine());
+            int cleaningCount = int.Parse(Console.ReadLine());
+            int discount = int.Parse(Console.ReadLine());
+            decimal totalPrice = penCount * penPrice + markerCount * markerPrice + cleaningCount * cleaningPrice;
+            totalPrice = totalPrice - totalPrice * (discount / 100m);
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
---
+++ Programming Basics with C#/First Steps in Programming - Exercise/VacationBookList/BookList.cs
@@ -0,0 +1,16 @@
+﻿namespace VacationBookList
+{
+    internal class BookList
+    {
+        static void Main(string[] args)
+        {
+
+            int pages = int.Parse(Console.ReadLine());
+            int pagesByHour = int.Parse(Console.ReadLine());
+            int daysToRead = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((pages / pagesByHour) / daysToRead);
+
+        }
+    }
+}
