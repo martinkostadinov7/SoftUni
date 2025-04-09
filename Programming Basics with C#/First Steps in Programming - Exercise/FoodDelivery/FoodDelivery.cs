4cb38f5422f427f0b1f0a940274757263e24648a
Martin Kostadinov <m.kostadinov07@gmail.com>
2025-04-09 20:20:04 +0300

    Add FoodDelivery and Repainting projects to solution
    Updated the solution file to include two new projects:
    FoodDelivery and Repainting. The global section was modified
    to support build configurations for these projects.
    
    Created FoodDelivery.cs to calculate total food prices
    based on user input for various food items. Added
    Repainting.cs to compute the total cost of materials
    and labor for a painting job.

---
+++ Programming Basics with C#/First Steps in Programming - Exercise/FoodDelivery/FoodDelivery.cs
@@ -0,0 +1,22 @@
+﻿    namespace FoodDelivery
+    {
+        internal class FoodDelivery
+        {
+            static void Main(string[] args)
+            {
+                decimal chickenPrice = 10.35m;
+                decimal fishPrice = 12.40m;
+                decimal vegetartianPrice = 8.15m;
+
+                int chickenQuantity = int.Parse(Console.ReadLine());
+                int fishQuantity = int.Parse(Console.ReadLine());
+                int vegetarianQuantity = int.Parse(Console.ReadLine());
+        
+            
+                decimal foodPrice = chickenPrice * chickenQuantity + fishPrice * fishQuantity + vegetartianPrice * vegetarianQuantity;
+                decimal desertPrice = foodPrice * 0.2m;
+                decimal totalPrice = foodPrice + desertPrice + 2.5m;
+                Console.WriteLine(totalPrice);
+            }
+        }
+    }
---
+++ Programming Basics with C#/First Steps in Programming - Exercise/Repainting/Repainting.cs
@@ -0,0 +1,27 @@
+﻿namespace Repainting
+{
+    internal class Repainting
+    {
+        static void Main(string[] args)
+        {
+            decimal naylonPrice = 1.50m;
+            decimal paintPrice = 14.50m;
+            decimal thinnerPrice = 5m;
+
+            int naylonQuantity = int.Parse(Console.ReadLine());
+            int paintQuantity = int.Parse(Console.ReadLine());
+            int thinnerQuantity = int.Parse(Console.ReadLine());
+            int hoursWorking = int.Parse(Console.ReadLine());
+
+            naylonQuantity += 2;
+            decimal newPaintQuantity = paintQuantity + paintQuantity * 0.1m;
+
+            decimal materialsPrice = 0.40m + (naylonQuantity * naylonPrice + newPaintQuantity * paintPrice + thinnerQuantity * thinnerPrice);
+            decimal workersPrice = (materialsPrice * 0.3m) * hoursWorking;
+
+            decimal totalPrice = materialsPrice + workersPrice;
+
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
