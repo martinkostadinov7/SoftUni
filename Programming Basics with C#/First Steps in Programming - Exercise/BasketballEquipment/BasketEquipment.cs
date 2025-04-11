107239968b1cab66e4b91dc870c9430b4e49e20f
Martin Kostadinov <m.kostadinov07@gmail.com>
2025-04-11 09:55:53 +0300

    Added exercises "BasketbalEquipment" and "FishTank".

---
+++ Programming Basics with C#/First Steps in Programming - Exercise/BasketballEquipment/BasketEquipment.cs
@@ -0,0 +1,18 @@
+﻿namespace BasketballEquipment
+{
+    internal class BasketEquipment
+    {
+        static void Main(string[] args)
+        {
+
+            int yearTax = int.Parse(Console.ReadLine());
+            decimal sneakersPrice = yearTax - yearTax * 0.4m;
+            decimal outfitPrice = sneakersPrice - sneakersPrice * 0.2m;
+            decimal ballPrice = outfitPrice / 4;
+            decimal accesoriesPrice = ballPrice / 5;
+
+            decimal totalSum = yearTax + sneakersPrice + outfitPrice + ballPrice + accesoriesPrice;
+            Console.WriteLine(totalSum);
+        }
+    }
+}
---
+++ Programming Basics with C#/First Steps in Programming - Exercise/FishTank/FishTank.cs
@@ -0,0 +1,20 @@
+﻿namespace FishTank
+{
+    internal class FishTank
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            decimal percent = decimal.Parse(Console.ReadLine());
+
+            decimal volume = (length * width * height) / 1000m;
+
+            decimal litersRequired = volume * (1 - (percent / 100));
+            Console.WriteLine(litersRequired);
+
+
+        }
+    }
+}
