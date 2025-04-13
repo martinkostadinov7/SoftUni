1d0618b130aaa21f08879f26f9d195161b75a5d1
martinkostadinov7 <m.kostadinov07@gmail.com>
2025-04-13 23:23:42 +0300

    added NumbersDivisibleBy5 and ComparingNumbersWithoutIfs exercises

---
+++ c#-book-nakov/Console Input and Output/ComparingNumbersWithoutIfs/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ComparingNumbersWithoutIfs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((a + b + Math.Abs(a - b)) / 2);
+        }
+    }
+}
---
+++ c#-book-nakov/Console Input and Output/NumbersDivisibleBy5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace NumbersDivisibleBy5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int nums = 0;
+            nums = (a % 5 == 0) || (b % 5 == 0) ? nums + 1 : nums += 0;
+            nums += (b - a) / 5;
+            Console.WriteLine(nums);
+        }
+    }
+}
