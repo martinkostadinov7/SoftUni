881820b37c01487c0e1540e527edf3e6f770ed05
martinkostadinov7 <m.kostadinov07@gmail.com>
2025-04-13 23:30:45 +0300

    Added 5NumbersSum and BiggestNumberOf5 exercises.

---
+++ c#-book-nakov/Console Input and Output/5NumbersSum/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _5NumbersSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, c, d, e;
+            bool isA = int.TryParse(Console.ReadLine(), out a);
+            bool isB = int.TryParse(Console.ReadLine(), out b);
+            bool isC = int.TryParse(Console.ReadLine(), out c);
+            bool isD = int.TryParse(Console.ReadLine(), out d);
+            bool isE = int.TryParse(Console.ReadLine(), out e);
+
+            int sum = a + b + c + d + e;
+            Console.WriteLine(sum);
+
+        }
+    }
+}
---
+++ c#-book-nakov/Console Input and Output/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BiggestNumberOf5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int e = int.Parse(Console.ReadLine());
+
+            int maxN = Math.Max(a, b);
+            int maxN1 = Math.Max(maxN, c);
+            int maxN2 = Math.Max(maxN1, d);
+            int maxN3 = Math.Max(maxN2, e);
+            Console.WriteLine(maxN3);
+        }
+    }
+}
