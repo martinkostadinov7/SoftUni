87617a29b933eb2ef62cdde61e4d5a9c949b3ab6
martinkostadinov7 <m.kostadinov07@gmail.com>
2025-04-13 23:16:02 +0300

    Added CompanyInfo and VirtualTableNumbers exercises.

---
+++ c#-book-nakov/Console Input and Output/CompanyInfo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CompanyInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string address = Console.ReadLine();
+            int phoneNumber = int.Parse(Console.ReadLine());
+            int faxNumber = int.Parse(Console.ReadLine());
+            string website = Console.ReadLine();
+            string managerName = Console.ReadLine();
+            string managerLatName = Console.ReadLine();
+            int managerPhoneNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Company info: Name: {name} \nAddress: {address} \nPhone number: {phoneNumber} \nFax: {faxNumber} \nWebsite: {website} \nManaged by {managerName} {managerLatName} with phone number: {managerPhoneNumber}");
+
+        }
+    }
+}
---
+++ c#-book-nakov/Console Input and Output/VirtualTableNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace VirtualTableNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 213;
+            decimal b = 21.9121m;
+            decimal c = -9.021921029m;
+
+            Console.WriteLine("|{0,-10:X}|{1,-10:F2}|{2,-10:F2}|", a, b, c);
+        }
+    }
+}
