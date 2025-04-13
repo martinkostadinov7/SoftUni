fd217969bc07c6a173668440b6454a0f0edc9d6c
martinkostadinov7 <m.kostadinov07@gmail.com>
2025-04-13 22:55:50 +0300

    Added ThreeNumbersSum and CircleCircumfrenceArea exercises.

---
+++ c#-book-nakov/Console Input and Output/CircleCircumfrenceArea/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CircleCircumfrenceArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int r = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The area of the circle is: " + (Math.PI * r * r));
+            Console.WriteLine("The circumfrence of the circle is: {0:F2}", (Math.PI * 2 * r));
+        }
+    }
+}
---
+++ c#-book-nakov/Console Input and Output/ThreeNumbersSum/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ThreeNumbersSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a + b + c);
+        }
+    }
+}
