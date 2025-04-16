433597a6f303bfc9d676da87eaeed97fea2a6289
martinkostadinov7 <m.kostadinov07@gmail.com>
2025-04-16 23:02:40 +0300

    Added exercises First100Fibonacci and FractionSum

---
+++ c#-book-nakov/Console Input and Output/First100Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace First100Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long n1 = 0;
+            long n2 = 1;
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            for (int i = 0; i < 100; i++)
+            {
+                long next = n1 + n2;
+                Console.WriteLine(next);
+                n1 = n2;
+                n2 = next;
+
+            }
+        }
+    }
+}
---
+++ c#-book-nakov/Console Input and Output/FractionSum/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FractionSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double oldSum = 0.0;
+            double currentSum = 0.0;
+            double precision = 0.001;
+            int i = 1;
+            while (true)
+            {
+                currentSum += 1.0 / i;
+
+                if (Math.Abs(currentSum - oldSum) < precision)
+                {
+                    break;
+                }
+                oldSum = currentSum;
+                i++;
+            }
+            Console.WriteLine(currentSum);
+
+        }
+    }
+}
