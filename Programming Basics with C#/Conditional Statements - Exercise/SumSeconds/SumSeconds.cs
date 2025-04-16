31b66f1c9b0ba9c422509ae59a0fe2d00a19f963
martinkostadinov7 <m.kostadinov07@gmail.com>
2025-04-16 23:16:38 +0300

    Added exercises Sum Seconds and Bonus Score

---
+++ Programming Basics with C#/Conditional Statements - Exercise/SumSeconds/SumSeconds.cs
@@ -0,0 +1,24 @@
+﻿namespace SumSeconds
+{
+    internal class SumSeconds
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            int totalSeconds = n1 + n2 + n3;
+            int minutes = totalSeconds / 60;
+            int seconds = totalSeconds % 60;
+            if (seconds < 10)
+            {
+                Console.WriteLine($"{minutes}:0{seconds}");
+            }
+            else
+            {
+                Console.WriteLine($"{minutes}:{seconds}");
+            }
+        }
+    }
+}
