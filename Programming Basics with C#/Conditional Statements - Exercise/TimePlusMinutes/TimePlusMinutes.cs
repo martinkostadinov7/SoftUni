8419039a1c31d6b4d03bb9af4f2ee11dd30135c7
martinkostadinov7 <m.kostadinov07@gmail.com>
2025-04-19 14:42:13 +0300

    Added Time + 15 Minutes and Toy Shop exerises.

---
+++ Programming Basics with C#/Conditional Statements - Exercise/TimePlusMinutes/TimePlusMinutes.cs
@@ -0,0 +1,34 @@
+﻿namespace TimePlusMinutes
+{
+    internal class TimePlusMinutes
+    {
+        static void Main(string[] args)
+        {
+
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 15;
+            if (minutes > 59)
+            {
+                minutes -= 60;
+                hours++;
+            }
+            if (hours > 23)
+            {
+                hours = 0;
+            }
+
+            Console.Write($"{hours}:");
+
+            if (minutes < 10)
+            {
+                Console.Write($"0{minutes}");
+            }
+            else
+            {
+                Console.Write($"{minutes}");
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/Conditional Statements - Exercise/ToyShop/ToyShop.cs
@@ -0,0 +1,51 @@
+﻿namespace ToyShop
+{
+//    Цена на екскурзията - реално число в интервала[1.00 … 10000.00]
+
+//2. Брой пъзели - цяло число в интервала[0… 1000]
+
+//3. Брой говорещи кукли - цяло число в интервала[0 … 1000]
+
+//4. Брой плюшени мечета - цяло число в интервала[0 … 1000]
+
+//5. Брой миньони - цяло число в интервала[0 … 1000]
+
+//6. Брой камиончета - цяло число в интервала[0 … 1000
+    internal class ToyShop
+    {
+        static void Main(string[] args)
+        {
+            decimal puzzlePrice = 2.6m;
+            decimal dollPrice = 3.0m;
+            decimal bearPrice = 4.1m;
+            decimal minionPrice = 8.2m;
+            decimal truckPrice = 2.0m;
+
+            decimal tourPrice = decimal.Parse(Console.ReadLine());
+            int puzzleCount = int.Parse(Console.ReadLine()); 
+            int dollCount = int.Parse(Console.ReadLine()); 
+            int bearCount = int.Parse(Console.ReadLine()); 
+            int minionCount = int.Parse(Console.ReadLine()); 
+            int truckCount = int.Parse(Console.ReadLine()); 
+
+            int totalToysCount = puzzleCount + dollCount + bearCount + minionCount + truckCount;
+            decimal totalToysPrice = puzzleCount * puzzlePrice + dollPrice * dollCount + bearPrice * bearCount + minionCount * minionPrice + truckPrice * truckCount;
+            if (totalToysCount >= 50)
+            {
+                totalToysPrice -= totalToysPrice * 0.25m;
+            }
+            totalToysPrice -= totalToysPrice * 0.1m;
+
+            if (tourPrice <= totalToysPrice)
+            {
+                decimal moneyLeft = totalToysPrice - tourPrice;
+                Console.WriteLine($"Yes! {moneyLeft:f2} lv left.");
+            }
+            else
+            {
+                decimal moneyLeft = tourPrice - totalToysPrice; 
+                Console.WriteLine($"Not enough money! {moneyLeft:f2} lv needed.");
+            }
+        }
+    }
+}
